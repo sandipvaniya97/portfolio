42727b8da2f18f2661f486f52808af39a0e17c2a
sandipvaniya97 <109646101+sandipvaniya97@users.noreply.github.com>
2022-09-13 07:09:38 +0530

    Update script.js

--- portfoilo js/script.js
+++ portfoilo js/script.js
@@ -1,46 +1,3 @@
-let menu = document.querySelector('#menu-bar');
-let navbar = document.querySelector('.navbar');
-
-menu.onclick = () =>{
-
-  menu.classList.toggle('fa-times');
-  navbar.classList.toggle('active');
-
-}
-
-window.onscroll = () =>{
-  
-  menu.classList.remove('fa-times');
-  navbar.classList.remove('active');
-
-  if(window.scrollY > 60){
-    document.querySelector('#scroll-top').classList.add('active');
-  }else{
-    document.querySelector('#scroll-top').classList.remove('active');
-  }
-
-}
-
-function loader(){
-  document.querySelector('.loader-container').classList.add('fade-out');
-}
-
-function fadeOut(){
-  setInterval(loader, 3000);
-}
-
-window.onload = fadeOut();
-
-const text = document.querySelector(".text");
-text.innerHTML = text.innerText
-	.split("")
-	.map(
-		(char, i) => `<span style="transform:rotate(${i * 10.3}deg)">${char}</span>`
-	)
-	.join("");  
-
-
-
 
   console.log(document.getElementById('progress'));
 document.getElementById("anchor-skill").addEventListener("click", () => {
@@ -48,7 +5,7 @@ document.getElementById("anchor-skill").addEventListener("click", () => {
   
   setTimeout(() => {
     document.getElementById('progress').classList.add("bar");
-  }, 100);
+  }, 500);
    
     
 
@@ -75,65 +32,39 @@ window.addEventListener(
 
 
 
+  $(document).ready(function(){
 
-//   var i = 0;
-// function move() {
-//   console.log('hi')
-//   if (i == 0) {
-//     i = 1;
-//     var elem = document.getElementById("progress");
-//     var width = 1;
-//     var id = setInterval(frame, 10);
-//     function frame() {
-//       if (width >= 70) {
-//         clearInterval(id);
-//         i = 0;
-//       } else {
-//         width++;
-//         elem.style.width = width + "%";
-//       }
-//     }
-//   }
-// }
+    $('#menu-bar').click(function(){
+      $(this).toggleClass('fa-times');
+      $('header').toggleClass('toggle'); 
+    });
+  
+    $(window).on('scroll load',function(){
+  
+      $('#menu-bar').removeClass('fa-times');
+      $('header').removeClass('toggle');
 
-
-
-
-  // $(document).ready(function(){
-
-  //   $('#menu-bar').click(function(){
-  //     $(this).toggleClass('fa-times');
-  //     $('header').toggleClass('toggle');
-  //   });
+      
   
-  //   $(window).on('scroll load',function(){
+      
   
-  //     $('#menu-bar').removeClass('fa-times');
-  //     $('header').removeClass('toggle');
+    });
   
-  //     if($(window).scroll-top() > 0){
-  //       $('.top').show();
-  //     }else{
-  //       $('.top').hide();
-  //     }
   
-  //   });
   
-  //   // smooth scrolling 
+    $('a[href*="#"]').on('click',function(e){
   
-  //   $('a[href*="#"]').on('click',function(e){
+      e.preventDefault();
   
-  //     e.preventDefault();
+      $('html, body').animate({
   
-  //     $('html, body').animate({
+        scrollTop : $($(this).attr('href')).offset().top,
   
-  //       scrollTop : $($(this).attr('href')).offset().top,
+      },
+        100, 
+        'linear'
+      );
   
-  //     },
-  //       500, 
-  //       'linear'
-  //     );
+    });
   
-  //   });
-  
-  // });
+  });
